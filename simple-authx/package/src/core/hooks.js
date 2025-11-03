8d7fb0b3ec7749883d9bc4fddfdcd7e01e0c4427
Antony Mwangi <antony254mm@gmail.com>
2025-11-03 23:03:23 +0300

    Delete simple-authx/package directory

--- simple-authx/package/src/core/hooks.js
+++
@@ -1,9 +0,0 @@
-// src/core/hooks.js
-export const defaultHooks = {
-  async onRegister(user) {
-    console.log(`[hook] User registered: ${user.username}`)
-  },
-  async onLogin(user) {
-    console.log(`[hook] User logged in: ${user.username}`)
-  }
-}
